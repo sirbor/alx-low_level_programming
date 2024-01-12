ee4343d13783a7e5d66ffeadce7dc198fefee3b0
Dominic Bor <dominicbor44@gmail.com>
2024-01-12 13:46:58 -0800

    0x0A. C - argc, argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- *main - prints all arguments it receives
+ *main - program that prints all arguments it receives.
  *@argc: number of arguments
- *@argv: array of arguments
- *Return: Always 0 (Success)
+ *@argv: array with the arguments
+ *Return: always 0
  */
+
 int main(int argc, char *argv[])
 {
-	int i;
+	int i = 0;
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
