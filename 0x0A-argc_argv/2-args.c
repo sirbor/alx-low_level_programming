67d2ca0d09cae1421d07b80241c35b279ed3dd65
Dominic Bor <dominicbor44@gmail.com>
2024-01-12 13:42:49 -0800

    0x0A. C - argc, argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- *maiin - prints all arguments it receives
+ *main - prints all arguments it receives
  *@argc: number of arguments
  *@argv: array of arguments
  *Return: Always 0 (Success)
