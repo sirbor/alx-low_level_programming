ae9622f977274a23726d44408f054597e991a4ab
Dominic Bor <dominicbor@icloud.com>
2025-05-26 14:18:10 +0300

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+  * main - prints "Programming is like building a multilingual puzzle,
+  * followed by a new line, to the standard output.
+  * Return: Always 0 (Success)
+  */
+int main(void) {
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
